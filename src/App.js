7be87ee619b7cbd9b792be05cdc0e4f14bc291f4
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-03-02 15:58:48 +0200

    login form visible always

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      user: null,
+      password: ''
     }
   }
 
@@ -14,11 +16,42 @@ class App extends React.Component {
     blogService.getAll().then(blogs =>
       this.setState({ blogs })
     )
-  } 
+  }
 
   render() {
+
+    const loginForm = () => (
+      <div>
+        <h2>Kirjaudu</h2>
+        
+        <form onSubmit={this.login}>
+          <div>
+            käyttäjätunnus
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            salasana
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button type="submit">kirjaudu</button>
+        </form>
+      </div>
+    )
+
     return (
       <div>
+        {loginForm()}
+
         <h2>blogs</h2>
         {this.state.blogs.map(blog => 
           <Blog key={blog._id} blog={blog}/>
