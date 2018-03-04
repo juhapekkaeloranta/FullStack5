37ea252f5a9ba246b26207d98461e20e4e414c02
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-03-04 14:13:24 +0200

    teht 5-7 like without backend

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+const baseUrl = '/api/blogs'
 
 class Blog extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      author: props.blog.author,
+      title: props.blog.title,
+      url: props.blog.url,
+      likes: props.blog.likes,
+      user: props.blog.user,
       visible: false
     }
   }
-
-  
 
   toggleVisibility = () => {
     this.setState({visible: !this.state.visible})
   }
 
+  likeBlog = async () => {
+    console.log('blog liked!')
+
+    this.setState({ likes: this.state.likes + 1 })
+
+    const likedBlog = {
+      author: this.state.author,
+      title: this.state.title,
+      url: this.state.url,
+      likes: this.state.likes,
+      user: this.state.user
+    }
+  }
+
   render() {
     //const hideWhenVisible = { display: this.state.visible ? 'none' : '' }
     const showWhenVisible = { display: this.state.visible ? '' : 'none' }
-
-    const blog = this.props.blog
 
     const blogStyle = {
       color: 'blue'
@@ -26,12 +43,12 @@ class Blog extends Component {
 
     return (
       <div>
-        <a onClick={this.toggleVisibility} style={blogStyle}>{blog.title}</a> - {blog.author}
+        <a onClick={this.toggleVisibility} style={blogStyle}>{this.state.title}</a> - {this.state.author}
         <div style={showWhenVisible}>
           <ul>
-            <li>{blog.url}</li>
-            <li>likes: {blog.likes}</li>
-            <li>added by: {blog.user}</li>
+            <li>{this.state.url}</li>
+            <li>likes: {this.state.likes} <button onClick={this.likeBlog}>Like</button></li>
+            <li>added by: {this.state.user}</li>
           </ul>
 
         </div>
