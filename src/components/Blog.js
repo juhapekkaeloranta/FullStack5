9f205dbd132226c66533145f9a353947a76f29d8
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-03-04 13:43:45 +0200

    teht 5-6 togglable blog info

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { Component } from 'react';
 
-export default Blog
+class Blog extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+
+  
+
+  toggleVisibility = () => {
+    this.setState({visible: !this.state.visible})
+  }
+
+  render() {
+    //const hideWhenVisible = { display: this.state.visible ? 'none' : '' }
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    const blog = this.props.blog
+
+    const blogStyle = {
+      color: 'blue'
+    }
+
+    return (
+      <div>
+        <a onClick={this.toggleVisibility} style={blogStyle}>{blog.title}</a> - {blog.author}
+        <div style={showWhenVisible}>
+          <ul>
+            <li>{blog.url}</li>
+            <li>likes: {blog.likes}</li>
+            <li>added by: {blog.user}</li>
+          </ul>
+
+        </div>
+      </div> 
+    )
+  }
+}
+
+export default Blog;
