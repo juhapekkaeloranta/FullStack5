6573486dd022f6a6fd0f59e11c1248becc5bc802
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-03-04 18:47:24 +0200

    teht 5-13 click test

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -9,10 +9,37 @@ describe.only('<SimpleBlog />', () => {
       title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
       likes: 0
     }
+    
+    const blogComponent = shallow(
+      <SimpleBlog 
+        blog={blog}
+      />
+    )
 
-    const blogComponent = shallow(<SimpleBlog blog={blog} />)
     const contentDiv = blogComponent.find('.blogHeader')
 
     expect(contentDiv.text()).toContain(blog.author)
+  })
+
+  it('clicking the button calls event handler once', () => {
+    const blog = {
+      author: 'Timo Testaaja',
+      title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+      likes: 0
+    }
+    const mockHandler = jest.fn()
+
+    const blogComponent = shallow(
+      <SimpleBlog 
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
